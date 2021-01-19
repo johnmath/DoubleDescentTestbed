818890322af758cbcae4f9d76d2898bea8ab6868
John Abascal <jaa17d@my.fsu.edu>
2021-01-19 05:08:18 +0000

    adding models class

---
+++ models.py
@@ -0,0 +1,9 @@
+class Models():
+    
+    def __init__(self):
+        pass
+    
+class MultilayerPerceptron(Models):
+    
+    def __init__(self):
+        super(MultilayerPerceptron, self).__init__()
