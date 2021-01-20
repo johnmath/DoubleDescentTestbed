f1dd8426e451f026053a26127952789ca3206fe0
John Abascal <jaa17d@my.fsu.edu>
2021-01-20 05:27:59 +0000

    Setting up module and creating first model

--- models.py
+++ models.py
@@ -1,9 +1,53 @@
-class Models():
+import torch.nn as nn
+import data
+
+class Models:
+    """This class contains the attributes that all models have in common.
     
-    def __init__(self):
-        pass
+    ...
+    
+    Attributes
+    ----------
+        loss : str
+            The loss function for the model. Options are {'L1', 'MSE', 
+            'CrossEntropy'}.
+        dataset : str
+            The dataset that the model will be trained on. Options are 
+            {'MNIST'}.
+        cuda : bool
+            If True, the model will train using GPU acceleration if a CUDA
+            GPU is available. If False, the model will train on CPU
+    """
+    
+    def __init__(self, loss, dataset):
+        
+        loss_functions = {'L1': nn.L1Loss(), 
+                          'MSE': nn.MSELoss(), 
+                          'CrossEntropy': nn.CrossEntropyLoss()}
+        
+        self.loss = loss_functions[loss]
+        self.dataset = dataset
+        
+        
     
 class MultilayerPerceptron(Models):
+    """A Multilayer Perceptron with a single hidden layer of variable size
     
-    def __init__(self):
-        super(MultilayerPerceptron, self).__init__()
+    ...
+    
+    Attributes
+    ----------
+        mlp_loss : str
+            The loss function for the model. Options are {'L1', 'MSE',
+            'CrossEntropy'}.
+        mlp_dataset : str
+            The dataset that the model will be trained on. Options are
+            {'MNIST'}.
+        cuda : bool
+            If True, the model will train using GPU acceleration if a CUDA
+            GPU is available. If False, the model will train on CPU
+    """
+    
+    def __init__(self, mlp_loss='MSE', mlp_dataset='MNIST'):
+        super(MultilayerPerceptron, self).__init__(mlp_loss, mlp_dataset)
+        
